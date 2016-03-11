efdab346e5e4fda28f19cca2e57e15d30fd2370e
Smasalykin <Smasalykin@it.ru>
2016-03-11 18:57:06 +0300

    4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import com.sun.jmx.remote.internal.Unmarshal;
+import com.sun.xml.internal.ws.util.Pool;
 import entity.DocumentsEntity;
 
 import javax.xml.bind.JAXBContext;
@@ -7,14 +9,29 @@ import java.io.File;
 
 public class Main {
 
+
+
+    static class Ddr extends Unmarshaller.Listener{
+        public Ddr(){
+            super();
+        }
+        @Override
+        public void afterUnmarshal(Object target, Object parent) {
+            System.out.println("cdcdcdcdc");
+            super.afterUnmarshal(target, parent);
+        }
+    }
+
+
+
     public static void main(String[] args) {
 
         try {
 
             File file = new File("D:\\dev\\tests\\testJaxB-master\\src\\test.xml");
             JAXBContext jaxbContext = JAXBContext.newInstance(DocumentsEntity.class);
-
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            jaxbUnmarshaller.setListener(new Ddr());
             DocumentsEntity documents = (DocumentsEntity) jaxbUnmarshaller.unmarshal(file);
             System.out.println(documents);
 
